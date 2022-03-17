3a50aa14b02a06e94594b13917066a9959d0ed8b
RubyAlvarez <rubalvarez@hugeinc.com>
2022-03-17 10:22:30 -0500

    practicing wai-aria, to make focus con tabindex in <Div> container of input

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,13 +1,14 @@
+/* eslint-disable jsx-a11y/aria-role */
 import '../components/InputBox.css';
 //mport idollar from '../images/idollar.svg';
+
 
 function InputBox(props){
     return(
         <div className="input-box">
           <label for={props.name}> {props.name}</label>
-					<div className="input-box__enter">
-					
-						<input type="number"id={props.name} placeholder="0" min="0"></input>
+					<div className="input-box__enter" role="input" tabIndex="0">
+						<input type="number"  id={props.name} placeholder="0" min="0"></input>
 					</div>
         </div>
     )
--- src/components/SelectTip.js
+++ src/components/SelectTip.js
@@ -1,16 +1,19 @@
+/* eslint-disable jsx-a11y/role-has-required-aria-props */
 import TipButton from "./TipBotton";
 import '../components/SelectTip.css';
 function SelectTip(){
     return(
-        <div className="container-select" >
+        <div className="container-select" tabIndex="0">
             Select Tip %
-            <div role="checkbox" aria-checked="false"  tabindex="0"className="select-tip">
+            <div role="combobox" className="select-tip">
             <TipButton name="5%"></TipButton>
             <TipButton name="10%"></TipButton>
             <TipButton name="15%"></TipButton>
             <TipButton name="25%"></TipButton>
             <TipButton name="50%"></TipButton>
-            <TipButton name="Custom"></TipButton>
+            <div className="tip-button custom">
+                <input placeholder="custom"/>
+            </div>
             </div>
             
         </div>
